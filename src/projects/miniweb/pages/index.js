f888c48c4b5574824995e80ed2ca698371302cce
Lefcott <lefcott@hotmail.com>
2021-06-14 12:55:24 -0300

    .

--- src/projects/miniweb/pages/index.js
+++ src/projects/miniweb/pages/index.js
@@ -1,53 +1,29 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import Layout from '../components/NewLanding/Layout';
+import PageTitle from '../components/NewLanding/PageTitle';
+import Slider from '../components/NewLanding/Slider';
+import Items from '../components/NewLanding/Items';
 
-import Banner from '../components/banner';
-import Paragraphs from '../components/paragraphs';
-import HowItWorks from '../components/HowItWorks';
-import Templates from '../components/Templates';
-import Clients from '../components/Clients';
-import Articles from '../components/Articles';
-import Contact from '../components/Contact';
-import Card from '../components/Card2';
-import { getSubdomains } from '../../../utils/url';
-import Spinner from '../../../components/Spinner';
-import { listItems } from '../../../services/api/item';
-
-import Layout from './layout';
-
-export default function Home() {
-  const project = useSelector(store => store.project);
-  const [card, setCard] = useState(null);
-  const [notFound, setNotFound] = useState(false);
-  const subdomains = typeof window === 'object' ? getSubdomains(window.location.hostname) : '';
-
-  useEffect(() => {
-    if (!project || !subdomains) return;
-
-    listItems('card', project, 1, 1, { filters: { subdomain: subdomains } }).then(({ data: givenCards }) => {
-      if (!givenCards.length) return setNotFound(true);
-      setCard(givenCards[0]);
-    });
-  }, [project, subdomains]);
-
-  if (notFound) return <h3>No encontramos la MiniWeb con subdominio {subdomains}</h3>;
-
-  if (subdomains) {
-    if (!card) return <Spinner />;
-    return <Card card={card} />;
-  }
-
-  return (
-    <div id="home">
-      <Layout>
-        <Banner />
-        <Paragraphs />
-        <HowItWorks />
-        <Templates />
-        <Clients />
-        <Articles />
-        <Contact />
-      </Layout>
+const NewLanding = () => (
+  <Layout>
+    <div className="section">
+      <PageTitle />
+      <Slider />
     </div>
-  );
-}
+    <Items />
+    <style jsx>
+      {`
+        .section {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          height: 100vh;
+          margin-bottom: 150px;
+          background-color: #281c5c;
+          overflow-x: hidden;
+        }
+      `}
+    </style>
+  </Layout>
+);
+
+export default NewLanding;
